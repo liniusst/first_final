54dd9408c9d590545a1f06719b476f5f9705c607
Linas Stonkus <liniusst@gmail.com>
2023-03-06 21:06:37 +0200

    update 03.06

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# 1. Nusirodome koks turi buti slaptazodzio ilgis from 8 to 12 (range)
-
 import random
 import string
 import logging
@@ -7,7 +5,7 @@ from typing import List
 
 logging.basicConfig(level=logging.DEBUG,filename='data.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
 
-class Generator:
+class PasswordGenerator:
     """Basic generator for all classes where get password lenght and base list by different password mode"""
 
     def __init__(self, password_len: int, base: List[str]) -> None:
@@ -23,50 +21,94 @@ class Generator:
         except Exception as error_code:
             print(error_code)
     
-class EasyToSay(Generator):
+class EasyToSay(PasswordGenerator):
     """Easy to say - Avoid numbers and special characters"""
 
     def __init__(self, password_len) -> None:
         base = string.ascii_letters
         super().__init__(password_len = password_len, base = base)
 
-class EasyToRead(Generator):
+class EasyToRead(PasswordGenerator):
     """Easy to read - Avoid special characters"""
 
     def __init__(self, password_len) -> None:
         base = string.ascii_letters + string.digits
         super().__init__(password_len = password_len, base = base)
 
-class StongPassword(Generator):
+class StongPassword(PasswordGenerator):
     """Stong password - Any numbers, chars and speial characters"""
 
     def __init__(self, password_len) -> None:
         base = string.ascii_letters + string.digits + string.punctuation
         super().__init__(password_len = password_len, base = base)
 
+# def final_password(mode: int, pwd_lenght: int) -> str:
+#     if mode == 1:
+#         password = EasyToSay(password_len= pwd_lenght)
+#         password = password.get_random_password()
+#     elif mode == 2:
+#         password = EasyToRead(password_len= pwd_lenght)
+#         password = password.get_random_password()
+#     elif mode == 3: 
+#         password = StongPassword(password_len= pwd_lenght)
+#         password = password.get_random_password()
+#     else:
+#         print("Only 3 modes are available!")
+#     return password
+
+# print('''Choose character set for password from these :
+#          1. Easy to say (Avoid numbers and special characters)
+#          2. Easy to read (Avoid special characters)
+#          3. Strong password (Any numbers, chars and speial characters)''')
+
+# try:
+#     input_password_lenght = int(input("Password lenght: "))
+#     selected_mode = int(input("Select password mode: "))
+#     print(final_password(mode= selected_mode, pwd_lenght= input_password_lenght))
+# except ValueError:
+#     print("klaida")
+
 
 print('''Choose character set for password from these :
-         1. Easy to say (Avoid numbers and special characters)
-         2. Easy to read (Avoid special characters)
-         3. Strong password (Any numbers, chars and speial characters)''')
+        1. Easy to say (Avoid numbers and special characters)
+        2. Easy to read (Avoid special characters)
+        3. Strong password (Any numbers, chars and speial characters)
+        4. Exit''')
 
+while True:
+    try:
+        selected_mode = int(input("Password mode: "))
+        input_password_lenght = int(input("Password lenght: "))
+        
+    except ValueError:
+        print("Select password mode 1-3 ant enter your choice")
+        continue
+        
+    # try:
+    #     input_password_lenght = int(input("Password lenght: "))
+    # except ValueError:
+    #     print("Password lenght. For example 8 or 12")
+    #     continue
 
-input_password_lenght = int(input("Password lenght: "))
-selected_mode = int(input("Select password mode: "))
+    try:
+        if selected_mode == 1:
+            password = EasyToSay(password_len= input_password_lenght)
+            password = password.get_random_password()
 
+        elif selected_mode == 2:
+            password = EasyToRead(password_len= input_password_lenght)
+            password = password.get_random_password()
 
-def final_password(mode, pwd_lenght):
-    if mode == 1:
-        password = EasyToSay(password_len= pwd_lenght)
-        print(f"Your password: {password.get_random_password()}")
-    elif mode == 2:
-        password = EasyToRead(password_len= pwd_lenght)
-        print(f"Your password: {password.get_random_password()}")
-    elif mode == 3: 
-        password = StongPassword(password_len= pwd_lenght)
-        print(f"Your password: {password.get_random_password()} ")
-    else:
-        print("Only 3 modes are available!")
-    return password
+        elif selected_mode == 3: 
+            password = StongPassword(password_len= input_password_lenght)
+            password = password.get_random_password()
+        else:
+            print("Only 3 modes are available!")
+    
+        print(password)
+        break    
+    except Exception as error_code:
+        logging.error(error_code)
 
-final_password(mode= selected_mode, pwd_lenght= input_password_lenght)
+        # print(password)
+        # break
